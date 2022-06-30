d22fe79ce9bf4a02a0a173aa8216651fe4c542e9
Yijun <yonghengyixin@gamil.com>
2022-06-29 21:17:58 -0400

    'logging'

--- src/service/main.py
+++ src/service/main.py
@@ -1,5 +1,8 @@
 # from turtle import st
-from fastapi import FastAPI
+from ast import If
+from random import random
+from fastapi import FastAPI, Request
+from requests import request
 from has_aircraft_in_given_x_y_coordinate import has_aircraft_in_given_x_y_coordinate
 from display_smallest_aircraft import display_smallest_aircraft
 from display_biggest_aircraft import display_biggest_aircraft
@@ -9,8 +12,30 @@ from return_images_with_maximum_airplanes import return_images_with_maximum_airp
 from return_images_with_truncated_aircraft import return_images_with_truncated_aircraft
 import json
 from collections import defaultdict
+import logging
+import time
+import random
+import string
+
+logging.config.fileConfig('logging.conf')
+
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
+
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    idem = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+    logger.info(f"rid={idem}||start request path={request.url.path}")
+    start_time = time.time()
+    
+    response = await call_next(request)
+    
+    process_time = (time.time() - start_time) * 1000
+    formatted_process_time = '{0:.2f}'.format(process_time)
+    logger.info(f"rid={idem}||completed_in={formatted_process_time}ms||status_code={response.status_code}")
+    
+    return response
 
 @app.get("/img/airplane/location")
 def HasAircraftInGivenLocation(x_loc: int, y_loc: int, image_id: str):
@@ -25,6 +50,10 @@ def HasAircraftInGivenLocation(x_loc: int, y_loc: int, image_id: str):
        image_id = "5c9e817a-dc4b-42ab-952c-3128e2de12e8.jpg"
         #todo
     result = has_aircraft_in_given_x_y_coordinate(image_id, x_loc, y_loc);
+    if(result == "No image found related to the image_id. Try effective image_id"):
+       logger.warn("Invalid Image ID Input")
+    elif(result == "Input X and Y should within (0,10)"):
+       logger.warn("Out of range number")
     return result;
 
 @app.get("/img/display/big")
@@ -34,6 +63,10 @@ async def DisplayBiggestAircraft(image_id: str, limit_of_number: int=5):
     Definition: On chosen image, display biggest airplanes with red bounding boxes, and return their coordinates
     """
     result = display_biggest_aircraft(image_id, limit_of_number)
+    if(result == "No image found related to the image_id. Try effective image_id"):
+       logger.warn("Invalid Image ID Input")
+    elif(result == "Input X and Y should within (0,2560)"):
+       logger.warn("Out of range number")
     return result;
 
 @app.get("/img/display/small")
@@ -42,7 +75,11 @@ async def DisplaySmallestAircraft(image_id: str, limit_of_number: int=5):
     
     Definition: On chosen image, display smallest airplanes with red bounding boxes, and return their coordinates
     """
-    result = display_smallest_aircraft(image_id, limit_of_number); 
+    result = display_smallest_aircraft(image_id, limit_of_number);
+    if(result == "No image found related to the image_id. Try effective image_id"):
+       logger.warn("Invalid Image ID Input")
+    elif(result == "Input X and Y should within (0,10)"):
+       logger.warn("Out of range number")
     return result;
 
 @app.get("/img/airplanes/count")
@@ -58,6 +95,8 @@ async def CountAirplanes(image_id: str):
               3) function will get the sum and return the number
     """
     result = count_airplanes_in_given_image(image_id)
+    if(result == "Sorry, we don't find the picture. Please check you enter info."):
+       logger.warn("Invalid Image ID Input")
     return result;
 
 @app.get("/img/airplanes/givenNumber")
@@ -78,6 +117,8 @@ async def ReturnImagesWithGivenNumberOfAircraft(contain_aircraft_number: int, li
                  if system didn't find match picture, function will return "Sorry, we don't find your needed"
     """
     result = return_images_with_given_number_of_aircraft(contain_aircraft_number, limit_of_image)
+    if(result == "The number should be positive" or result == "Sorry, we don't find your needed" or result == "limit_of_image should between [1,10]"):
+       logger.warn("Out of range number")
     return result;
 
 @app.get("/img/airplanes/maximum")
@@ -95,6 +136,8 @@ async def ReturnImagesWithMaximumAircraft(number_of_image: int=1):
                  function will return top X pictures
     """
     result = return_images_with_maximum_airplanes(number_of_image)
+    if(result == "limit_of_image should between [1,10]"):
+       logger.warn("Out of range number")
     return result;
 
 @app.get("/img/airplanes/truncated")
@@ -112,4 +155,6 @@ async def ReturnImageWithTruncatedAircraft(number_of_image: int=3):
                  function will return top X pictures
     """
     result = return_images_with_truncated_aircraft(number_of_image)
+    if(result == "limit_of_image should between [1,10]"):
+       logger.warn("Out of range number")
     return result;
