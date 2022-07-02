d5fd0ade1640c4bc471b0c7bdf3e937a5b4ee784
Zhijie-Lii <l445476530@yahoo.com>
2022-07-01 21:33:29 -0400

    Icon

---
+++ streamlit/pages/📊 Profiling.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import pickle
+from pathlib import Path
+import streamlit_authenticator as stauth
+
+names = ["zhijie_li", "yijun_lin", "damg7245_team4", "parth_shah", "srikanth_krishnamurthy"]
+usernames = ["zhijie", "yijun", "team4", "parth", "srikanth"]
+
+file_path = Path(__file__).parent.parent / "streamlitUserPW.pkl"
+with file_path.open("rb") as file:
+    hashed_passwords = pickle.load(file)
+
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords, "streamlitMain", "abcdef", cookie_expiry_days=0)
+
+if st.session_state["authentication_status"]:
+    authenticator.logout('Logout', 'sidebar')
+    st.markdown('# Panda Profiling Page')
+    HtmlFile = open("../reports/airplane_data_profiling.html", 'r')
+    source_code = HtmlFile.read() 
+    print(source_code)
+    components.html(source_code, height = 13200)
+else:
+    st.markdown('# Please go to streamlitMain login')
---
+++ streamlit/pages/🚥 pytest.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pickle
+from pathlib import Path
+import streamlit_authenticator as stauth
+
+st.title(" Unit tests for APIs")
+
+names = ["zhijie_li", "yijun_lin", "damg7245_team4", "parth_shah", "srikanth_krishnamurthy"]
+usernames = ["zhijie", "yijun", "team4", "parth", "srikanth"]
+
+file_path = Path(__file__).parent.parent / "streamlitUserPW.pkl"
+with file_path.open("rb") as file:
+    hashed_passwords = pickle.load(file)
+
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords, "streamlitMain", "abcdef", cookie_expiry_days=0)
+
+if st.session_state["authentication_status"]:
+
+    st.subheader('Username')
+    authenticator.logout('Logout', 'sidebar')
+
+else:
+    st.markdown('# Please go to streamlitMain login')    
